6ced89afd8abcf0ec6dce1516a5dbedd92a3f2db
darabosmate <darabos2001@gmail.com>
2023-03-25 17:30:47 +0100

    comments deleted for visibility

--- HAZI/HAZI04/HAZI04.py
+++ HAZI/HAZI04/HAZI04.py
@@ -1,204 +1,49 @@
-# %%
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-#import random
+import random
 
-# %%
-'''
-FONTOS: Az első feladatáltal visszaadott DataFrame-et kell használni a további feladatokhoz. 
-A függvényeken belül mindig készíts egy másolatot a bemenő df-ről, (new_df = df.copy() és ezzel dolgozz tovább.)
-'''
 
-# %%
-'''
-Készíts egy függvényt, ami egy string útvonalat vár paraméterként, és egy DataFrame ad visszatérési értékként.
-
-Egy példa a bemenetre: 'test_data.csv'
-Egy példa a kimenetre: df_data
-return type: pandas.core.frame.DataFrame
-függvény neve: csv_to_df
-'''
-
-# %%
 def csv_to_df(path:str)->pd.core.frame.DataFrame:
     ndf = pd.read_csv(path)
     return ndf
 
-#df = csv_to_df('StudentsPerformance.csv')
+def capitalize_columns(df_data:pd.DataFrame) -> pd.core.frame.DataFrame:
+    new_df = df_data.copy()
+    new_column_names = new_df.columns.to_series().apply(lambda x: x.upper() if 'e' not in x.lower() else x)
+    new_df.rename(columns=new_column_names, inplace=True)
+    return new_df
 
-# %%
-'''
-Készíts egy függvényt, ami egy DataFrame-et vár paraméterként, 
-és átalakítja azoknak az oszlopoknak a nevét nagybetűsre amelyiknek neve nem tartalmaz 'e' betüt.
-
-Egy példa a bemenetre: df_data
-Egy példa a kimenetre: df_data_capitalized
-return type: pandas.core.frame.DataFrame
-függvény neve: capitalize_columns
-'''
-
-# %%
-def capitalize_columns(df:pd.core.frame.DataFrame)->pd.core.frame.DataFrame:
-    ndf = df.copy()
-    ndf = ndf.rename(columns=renaming_str)
-    return ndf
-
-def renaming_str(x:str):
-    if "e" in x or "E" in x:
-        return x.upper()
-    return x
-
-#capitalize_columns(df)
-
-
-
-# %%
-'''
-Készíts egy függvényt, ahol egy szám formájában vissza adjuk, hogy hány darab diáknak sikerült teljesíteni a matek vizsgát.
-(legyen az átmenő ponthatár 50).
-
-Egy példa a bemenetre: df_data
-Egy példa a kimenetre: 5
-return type: int
-függvény neve: math_passed_count
-'''
-
-# %%
 def math_passed_count(df_data: pd.core.frame.DataFrame)-> int:
     ndf = df_data.copy()
     return sum(ndf["math score"]>50)
 
-#math_passed_count(df)
-
-# %%
-'''
-Készíts egy függvényt, ahol Dataframe ként vissza adjuk azoknak a diákoknak az adatait (sorokat), akik végeztek előzetes gyakorló kurzust.
-
-Egy példa a bemenetre: df_data
-Egy példa a kimenetre: df_did_pre_course
-return type: pandas.core.frame.DataFrame
-függvény neve: did_pre_course
-'''
-
-# %%
 def did_pre_course(df: pd.DataFrame)->pd.core.frame.DataFrame:
     ndf = df.copy()
     return ndf.loc[ndf["test preparation course"] == "completed"]
 
-#did_pre_course(df)
-
-
-# %%
-'''
-Készíts egy függvényt, ahol a bemeneti Dataframet a diákok szülei végzettségi szintjei alapján csoportosításra kerül,
-majd aggregációként vegyük, hogy átlagosan milyen pontszámot értek el a diákok a vizsgákon.
-
-Egy példa a bemenetre: df_data
-Egy példa a kimenetre: df_average_scores
-return type: pandas.core.frame.DataFrame
-függvény neve: average_scores
-'''
-
-# %%
 def average_scores(df: pd.DataFrame)->pd.core.frame.DataFrame:
     ndf = df.copy()
     return ndf.groupby(["parental level of education"]).mean()
 
-#average_scores(df)
-
-# %%
-'''
-Készíts egy függvényt, ami a bementeti Dataframet kiegészíti egy 'age' oszloppal, töltsük fel random 18-66 év közötti értékekkel.
-A random.randint() függvényt használd, a random sorsolás legyen seedleve, ennek értéke legyen 42.
-
-Egy példa a bemenetre: df_data
-Egy példa a kimenetre: df_data_with_age
-return type: pandas.core.frame.DataFrame
-függvény neve: add_age
-'''
-
-# %%
 def add_age(df: pd.DataFrame)->pd.core.frame.DataFrame:
     ndf = df.copy()
     np.random.seed(42)
     ndf["age"] = np.random.randint(18, 66)
     return ndf
 
-#add_age(df)
-
-# %%
-'''
-Készíts egy függvényt, ami vissza adja a legjobb teljesítményt elérő női diák pontszámait.
-
-Egy példa a bemenetre: df_data
-Egy példa a kimenetre: (99,99,99) #math score, reading score, writing score
-return type: tuple
-függvény neve: female_top_score
-'''
-
-# %%
 def female_top_score(df: pd.DataFrame)->tuple:
     ndf = df.copy()
     return tuple(ndf.loc[ndf["gender"] == "female"].max())[5:]
 
-#female_top_score(df)
+def add_grade(df_data:pd.DataFrame) -> pd.core.frame.DataFrame:
+    new_df = df_data.copy()
+    new_df['percent'] = (new_df['math score'] + new_df['reading score'] + new_df['writing score']) / 300
+    new_df['grade'] = pd.cut(new_df['percent'], bins=[0, 0.6, 0.7, 0.8, 0.9, 1], labels=['F', 'D', 'C', 'B', 'A'])
+    new_df.drop('percent', axis=1, inplace=True)
+    return new_df
 
-# %%
-'''
-Készíts egy függvényt, ami a bementeti Dataframet kiegészíti egy 'grade' oszloppal. 
-Számoljuk ki hogy a diákok hány százalékot ((math+reading+writing)/300) értek el a vizsgán, és osztályozzuk őket az alábbi szempontok szerint:
-
-90-100%: A
-80-90%: B
-70-80%: C
-60-70%: D
-<60%: F
-
-Egy példa a bemenetre: df_data
-Egy példa a kimenetre: df_data_with_grade
-return type: pandas.core.frame.DataFrame
-függvény neve: add_grade
-'''
-
-# %%
-def add_grade(df: pd.core.frame.DataFrame)->pd.core.frame.DataFrame:
-    ndf = df.copy()
-    ndf["grade"] = ndf.apply(lambda row: grade((row.at["math score"] + row["writing score"] +row["reading score"]) / 300), axis=1)
-    return ndf
-
-def grade(score: float)->str:
-    if score >= 0.9:
-        return 'A'
-    elif score >= 0.8:
-        return 'B'
-    elif score >= 0.7:
-        return 'C'
-    elif score >= 0.6:
-        return 'D'
-    else:
-        return 'F'
-
-#add_grade(df)
-
-
-
-# %%
-'''
-Készíts egy függvényt, ami a bemeneti Dataframe adatai alapján elkészít egy olyan oszlop diagrammot,
-ami vizualizálja a nemek által elért átlagos matek pontszámot.
-
-Oszlopdiagram címe legyen: 'Average Math Score by Gender'
-Az x tengely címe legyen: 'Gender'
-Az y tengely címe legyen: 'Math Score'
-
-Egy példa a bemenetre: df_data
-Egy példa a kimenetre: fig
-return type: matplotlib.figure.Figure
-függvény neve: math_bar_plot
-'''
-
-# %%
 #TODO
 def math_bar_plot(df:pd.DataFrame)-> plt.Figure:
     ndf = df.copy()
@@ -212,24 +57,6 @@ def math_bar_plot(df:pd.DataFrame)-> plt.Figure:
     
     return fig
 
-#math_bar_plot(df)
-
-# %%
-''' 
-Készíts egy függvényt, ami a bemeneti Dataframe adatai alapján elkészít egy olyan histogramot,
-ami vizualizálja az elért írásbeli pontszámokat.
-
-A histogram címe legyen: 'Distribution of Writing Scores'
-Az x tengely címe legyen: 'Writing Score'
-Az y tengely címe legyen: 'Number of Students'
-
-Egy példa a bemenetre: df_data
-Egy példa a kimenetre: fig
-return type: matplotlib.figure.Figure
-függvény neve: writing_hist
-'''
-
-# %%
 def writing_hist(df: pd.frame.DataFrame)->plt.Figure:
     ndf = df.copy()
     x = ndf['writing score']
@@ -241,25 +68,3 @@ def writing_hist(df: pd.frame.DataFrame)->plt.Figure:
     ax.set_title('Distribution of Writing Scores')
     
     return fig
-
-#writing_hist(df)
-
-# %%
-''' 
-Készíts egy függvényt, ami a bemeneti Dataframe adatai alapján elkészít egy olyan kördiagramot,
-ami vizualizálja a diákok etnikum csoportok szerinti eloszlását százalékosan.
-
-Érdemes megszámolni a diákok számát, etnikum csoportonként,majd a százalékos kirajzolást az autopct='%1.1f%%' paraméterrel megadható.
-Mindegyik kör szelethez tartozzon egy címke, ami a csoport nevét tartalmazza.
-A diagram címe legyen: 'Proportion of Students by Race/Ethnicity'
-
-Egy példa a bemenetre: df_data
-Egy példa a kimenetre: fig
-return type: matplotlib.figure.Figure
-függvény neve: ethnicity_pie_chart
-'''
-
-# %%
-
-
-
