7dab25745dcf787b5a3c0d0fdf0a2af35c4a8d71
darabosmate <mate.darabos@stud.uni-obuda.hu>
2023-03-28 19:30:30 +0200

    GYAK05 fixed

--- GYAK/GYAK05/GYAK05.py
+++ GYAK/GYAK05/GYAK05.py
@@ -6,35 +6,52 @@ from scipy.stats import mode
 from sklearn.metrics import confusion_matrix
 
 
-csv_path = "archive/iris.data.csv"
-
 class KNNClassifier:
 
-    k_neighbors = 0
+    def k_neighbors(self):
+        return self.k
 
-    def __init__(self, k:int, test_split_ratio:float)-> None:
+    def __init__(self, k: int, test_split_ratio: float) -> None:
         self.k = k
         self.test_split_ratio = test_split_ratio
-    
-    def train_test_split(self, features:np.ndarray, labels:np.ndarray) -> None:
+
+        self.x_test, self.y_test = None, None
+        self.x_train, self.y_train = None, None
+        self.y_preds = None
+
+    def train_test_split(self, features: np.ndarray, labels: np.ndarray) -> None:
         test_size = int(len(features) * self.test_split_ratio)
         train_size = len(features) - test_size
         assert len(features) == test_size + train_size, "Size mismatch!"
 
-        self.x_train, self.y_train = features[:train_size,:],labels[:train_size]
-        self.x_test, self.y_test = features[train_size:train_size+test_size,:], labels[train_size:train_size + test_size]
-        #return (x_train,y_train,x_test,y_test)  
+        self.x_train, self.y_train = features[:train_size, :], labels[:train_size]
+        self.x_test, self.y_test = features[train_size:train_size+test_size, :], labels[train_size:train_size + test_size]
 
-    def euclidean(self, points:np.ndarray,element_of_x:np.ndarray) -> np.ndarray:
-    return np.sqrt(np.sum((points - element_of_x)**2,axis=1))
+    def euclidean(self, element_of_x: np.ndarray) -> np.ndarray:
+        return np.sqrt(np.sum((self.x_train - element_of_x)**2, axis=1))
 
+    def predict(self, x_test: np.ndarray) -> None:
+        labels_pred = []
+        for x_test_element in x_test:
+            distances = self.euclidean(x_test_element)
+            distances = np.array(sorted(zip(distances, self.y_train)))
+            label_pred = mode(distances[:self.k, 1], keepdims=False).mode
+            labels_pred.append(label_pred)
+        self.y_preds = labels_pred
 
+    def accuracy(self) -> float:
+        true_positive = (self.y_test == self.y_preds).sum()
+        return true_positive / len(self.y_test) * 100
 
-
+    def confusion_matrix(self):
+        conf_matrix = confusion_matrix(self.y_test, self.y_preds)
+        sns.heatmap(conf_matrix, annot=True)
 
     @staticmethod
-    def load_csv(csv_path:str) ->Tuple[np.ndarray,np.ndarray]:
-        df = pd.read_csv("archive/iris.data.csv")
-        df = df.sample(frac=1).reset_index(drop=True)
-        x,y = dataset[:,:4],dataset[:,-1]
-        return x,y
+    def load_csv(csv_path: str) -> Tuple[np.ndarray, np.ndarray]:
+        np.random.seed(42)
+        dataset = np.genfromtxt(csv_path, delimiter=',')
+        np.random.shuffle(dataset)
+        x_, y_ = dataset[:, :4], dataset[:, -1]
+        return x_, y_
+
