6b113b7665c28daf4b2d90f4730f356b720f0455
darabosmate <mate.darabos@stud.uni-obuda.hu>
2023-04-01 23:14:35 +0200

    pred, best_k

--- HAZI/HAZI05/HAZI05.py
+++ HAZI/HAZI05/HAZI05.py
@@ -39,27 +39,27 @@ class KNNClassifier:
         assert features.shape[0] == test_size + train_size, "Size mismatch!"
 
         self.x_train, self.y_train = features.iloc[:train_size,
-                                                   :], labels.iloc[:train_size]
+                                                   :], labels.iloc[:train_size].reset_index(drop=True)
 
         self.x_test, self.y_test = features.iloc[train_size:train_size +
-                                                 test_size, :], labels.iloc[train_size:train_size + test_size]
+                                                 test_size, :], labels.iloc[train_size:train_size + test_size].reset_index(drop=True)
 
     def euclidean(self, element_of_x: pd.DataFrame) -> pd.DataFrame:  # 4
 
         return np.sqrt(np.sum((self.x_train - element_of_x)**2, axis=1))
 
-    def predict(self, x_test: pd.core.frame.DataFrame):
-        labels_pred = []
-        for index, x_test_element in x_test.iterrows():
-            distances = self.euclidean(x_test_element)
-            distances = pd.DataFrame(sorted(zip(distances, self.y_train)))
-            label_pred = distances.iloc[:self.k, 1].mode()
-            labels_pred.append(label_pred)
 
-        self.y_preds = pd.DataFrame(labels_pred).iloc[:, 0]
+    def predict(self,x_test:pd.DataFrame) -> pd.Series: #5
+        labels_pred = []
+        for idx, x_test_element in x_test.iterrows():
+            distances = self.euclidean(x_test_element)
+            distances = pd.concat([distances, self.y_train], axis=1).sort_values(by=0)
+            label_pred = mode(distances.iloc[:self.k,1],keepdims=False).mode
+            labels_pred.append(label_pred)
+        self.y_preds = pd.DataFrame(labels_pred)
 
     def accuracy(self) -> float:  # 6
-        true_positive = (self.y_test == pd.DataFrame(self.y_preds)).sum()
+        true_positive = (self.y_test.iloc[:, 0] == self.y_preds.iloc[:, 0]).sum()
 
         return true_positive / self.y_test.shape[0] * 100
 
@@ -67,6 +67,17 @@ class KNNClassifier:
         conf_matrix = confusion_matrix(self.y_test, self.y_preds)
 
         return conf_matrix
+
+    def best_k(self):
+        ret = tuple((1, -1))
+        for i in range(1, 21):
+            self.k = i
+            self.predict(self.x_test)
+            acc = self.accuracy()
+            if acc > ret[1]:
+                ret = (self.k, acc.round(2))
+
+        return ret
 
     @staticmethod
     def load_csv(csv_path: str) -> Tuple[pd.DataFrame, pd.DataFrame]:  # 2
