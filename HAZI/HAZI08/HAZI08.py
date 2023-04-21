dea1025994aae4eb72bda005f601bc4b0ccc33d3
darabosmate <mate.darabos@stud.uni-obuda.hu>
2023-04-21 11:52:14 +0200

    HAZI08 created

---
+++ HAZI/HAZI08/HAZI08.py
@@ -0,0 +1,167 @@
+# %%
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.metrics import mean_squared_error
+
+# %%
+'''
+Készíts egy függvényt, betölti majd vissza adja az iris adathalmazt.
+
+
+Egy példa a kimenetre: iris
+return type: sklearn.utils.Bunch
+függvény neve: load_iris_data
+'''
+
+# %%
+def load_iris_data() : #-> sklearn.utils.Bunch, -> sklearn.utils._bunch.Bunch
+    iris = load_iris()
+    return iris
+
+iris_data = load_iris_data()
+#iris_data
+
+# %%
+'''
+Készíts egy függvényt, ami a betölti az virágokhoz tartozó levél méretket egy dataframebe, majd az első 5 sort visszaadja.
+Minden oszlop tartalmazza, hogy az milyen mérethez tartozik.
+
+Egy példa a bemenetre: iris
+Egy példa a kimenetre: iris_df
+return type: pandas.core.frame.DataFrame
+függvény neve: check_data
+'''
+
+# %%
+def check_data(iris)-> pd.core.frame.DataFrame:
+    df = pd.DataFrame(iris.data) #, columns=[iris['sepal length (cm)', 'sepal width (cm)']]
+    df.columns = iris.feature_names
+    return df.iloc[:, :2].head(5)
+
+#check_data(iris_data)
+
+# %%
+''' 
+Készíts egy függvényt ami előkészíti az adatokat egy lineaáris regressziós model feltanításához.
+Featurejeink legyenek a levél méretek kivéve a "sepal length (cm)", ez legyen a targetünk.
+
+Egy példa a bemenetre: iris
+Egy példa a kimenetre: X, y
+return type: (numpy.ndarray, numpy.ndarray)
+függvény neve: linear_train_data
+'''
+
+# %%
+def linear_train_data(input)-> (np.ndarray, np.ndarray):
+    df = pd.DataFrame(input.data) #, columns=[iris['sepal length (cm)', 'sepal width (cm)']]
+    df.columns = input.feature_names
+    df.drop(columns=['petal length (cm)', 'petal width (cm)'], inplace=True)
+    
+    return (df.iloc[:, 0].to_numpy(), df.iloc[:, 1].to_numpy())
+    
+
+# %%
+''' 
+Készíts egy függvényt ami előkészíti az adatokat egy logisztikus regressziós model feltanításához.
+Featurejeink legyenek a levél méretek, targetünk pedig a 0, 1-es virág osztályok.
+Fontos csak azokkal az adatokkal tanítsunk amihez tartozik adott target. 
+
+Egy példa a bemenetre: iris
+Egy példa a kimenetre: X, y
+return type: (numpy.ndarray, numpy.ndarray)
+függvény neve: logistic_train_data
+'''
+
+# %%
+
+
+# %%
+'''
+Készíts egy függvényt ami feldarabolja az adatainkat train és test részre. Az adatok 20%-át használjuk fel a teszteléshez.
+Tegyük determenisztikussá a darabolást, ennek értéke legyen 42.
+
+Egy példa a bemenetre: X, y
+Egy példa a kimenetre: X_train, X_test, y_train, y_test
+return type: (numpy.ndarray, numpy.ndarray, numpy.ndarray, numpy.ndarray)
+függvény neve: split_data
+'''
+
+# %%
+
+
+# %%
+'''
+Készíts egy függvényt ami feltanít egy lineaáris regressziós modelt, majd visszatér vele.
+
+Egy példa a bemenetre: X_train, y_train
+Egy példa a kimenetre: model
+return type: sklearn.linear_model._base.LinearRegression
+függvény neve: train_linear_regression
+'''
+
+# %%
+
+
+# %%
+'''
+Készíts egy függvényt ami feltanít egy logisztikus regressziós modelt, majd visszatér vele.
+
+Egy példa a bemenetre: X_train, y_train
+Egy példa a kimenetre: model
+return type: sklearn.linear_model._base.LogisticRegression
+függvény neve: train_logistic_regression
+'''
+
+# %%
+
+
+# %%
+''' 
+Készíts egy függvényt, ami a feltanított modellel predikciót tud végre hajtani.
+
+Egy példa a bemenetre: model, X_test
+Egy példa a kimenetre: y_pred
+return type: numpy.ndarray
+függvény neve: predict
+'''
+
+# %%
+
+
+# %%
+'''
+Készíts egy függvényt, ami vizualizálni tudja a label és a predikciók közötti eltérést.
+Használj scatter plotot a diagram elkészítéséhez.
+
+Diagram címe legyen: 'Actual vs Predicted Target Values'
+Az x tengely címe legyen: 'Actual'
+Az y tengely címe legyen: 'Predicted'
+
+Egy példa a bemenetre: y_test, y_pred
+Egy példa a kimenetre: scatter plot
+return type: matplotlib.figure.Figure
+függvény neve: plot_actual_vs_predicted
+'''
+
+# %%
+
+
+# %%
+''' 
+Készíts egy függvényt, ami a Négyzetes hiba (MSE) értékét számolja ki a predikciók és a valós értékek között.
+
+Egy példa a bemenetre: y_test, y_pred
+Egy példa a kimenetre: mse
+return type: float
+függvény neve: evaluate_model
+'''
+
+# %%
+
+
+
