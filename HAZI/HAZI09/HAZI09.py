112e4fbc2cb3ba47451388f386f804613f5a0d20
darabosmate <mate.darabos@stud.uni-obuda.hu>
2023-04-30 11:46:53 +0200

    hazi09 finished

--- HAZI/HAZI09/HAZI09.py
+++ HAZI/HAZI09/HAZI09.py
@@ -1,5 +1,7 @@
 import numpy as np
 from sklearn import datasets
+from scipy.stats import mode
+from numpy import ma
 
 class KMeansOnDigits:
     def __init__(self,  n_clusters=10, random_state=0):
@@ -12,12 +14,13 @@ class KMeansOnDigits:
     
     def predict(n_clusters:int,random_state:int,digits)-> (KMeans, np.ndarray):
         km = KMeans(n_clusters=n_clusters, random_state=random_state)
-        self.clusters = km.fit_predict(digits)
-        
+        self.clusters = km.fit_predict(self.digits.data)
+
     def get_labels(clusters:np.ndarray, digits)-> np.ndarray:
         result = np.zeros(clusters.shape[0])
+        for i in range(10):
+            ma_arr = ma.masked_equal(self.clusters, i)
+            mod = mode(self.digits.target[mask], keepdims=False).mode
+            result[ma_arr.mask] = mod
 
         return result
-
-
-
