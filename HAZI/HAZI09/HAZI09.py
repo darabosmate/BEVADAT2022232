a728f4a3835c8fbd78c10d5fafe53e47c0829174
darabosmate <mate.darabos@stud.uni-obuda.hu>
2023-05-01 21:40:34 +0200

    accuracy + confusion matrix added

--- HAZI/HAZI09/HAZI09.py
+++ HAZI/HAZI09/HAZI09.py
@@ -2,6 +2,9 @@ import numpy as np
 from sklearn import datasets
 from scipy.stats import mode
 from numpy import ma
+from sklearn.cluster import KMeans
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
 
 class KMeansOnDigits:
     def __init__(self,  n_clusters=10, random_state=0):
@@ -12,9 +15,11 @@ class KMeansOnDigits:
     def load_dataset(self):
         self.digits = datasets.load_digits()
     
+
     def predict(n_clusters:int,random_state:int,digits)-> (KMeans, np.ndarray):
         km = KMeans(n_clusters=n_clusters, random_state=random_state)
         self.clusters = km.fit_predict(self.digits.data)
+
 
     def get_labels(clusters:np.ndarray, digits)-> np.ndarray:
         result = np.zeros(clusters.shape[0])
@@ -24,3 +29,11 @@ class KMeansOnDigits:
             result[ma_arr.mask] = mod
 
         return result
+
+
+    def calc_accuracy(self):
+        self.accuracy = np.round(accuracy_score(self.digits.target, self.clusters), decimals=2)
+
+
+    def confusion_matrix(self):
+        self.mat = conf_matrix(self.digits.target, self.clusters)
