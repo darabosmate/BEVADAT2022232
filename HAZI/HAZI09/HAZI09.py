6fad9936c9169be26887f8d5195756544e394cce
darabosmate <mate.darabos@stud.uni-obuda.hu>
2023-05-01 21:48:41 +0200

    unnecessary function params removed

--- HAZI/HAZI09/HAZI09.py
+++ HAZI/HAZI09/HAZI09.py
@@ -5,6 +5,7 @@ from numpy import ma
 from sklearn.cluster import KMeans
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
+import seaborn as sns
 
 class KMeansOnDigits:
     def __init__(self,  n_clusters=10, random_state=0):
@@ -16,19 +17,19 @@ class KMeansOnDigits:
         self.digits = datasets.load_digits()
     
 
-    def predict(n_clusters:int,random_state:int,digits)-> (KMeans, np.ndarray):
+    def predict(self):
         km = KMeans(n_clusters=n_clusters, random_state=random_state)
         self.clusters = km.fit_predict(self.digits.data)
 
 
-    def get_labels(clusters:np.ndarray, digits)-> np.ndarray:
+    def get_labels(self):
         result = np.zeros(clusters.shape[0])
         for i in range(10):
             ma_arr = ma.masked_equal(self.clusters, i)
             mod = mode(self.digits.target[mask], keepdims=False).mode
             result[ma_arr.mask] = mod
 
-        return result
+        self.labels = result
 
 
     def calc_accuracy(self):
