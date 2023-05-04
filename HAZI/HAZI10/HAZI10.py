6f5315c9f067f37282263bf89371285914dd2029
darabosmate <mate.darabos@stud.uni-obuda.hu>
2023-05-04 21:11:31 +0200

    hazi10 finished😤

---
+++ HAZI/HAZI10/HAZI10.py
@@ -0,0 +1,66 @@
+import tensorflow as tf
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses import SparseCategoricalCrossentropy
+'''
+Készíts egy metódust ami a mnist adatbázisból betölti a train és test adatokat. (tf.keras.datasets.mnist.load_data())
+Majd a tanitó, és tesztelő adatokat normalizálja, és vissza is tér velük.
+Egy példa a kimenetre: train_images, train_labels, test_images, test_labels
+függvény neve: mnist_digit_data
+'''
+def mnist_digit_data():
+    (X_train, y_train), (X_test, y_test) = tf.keras.datasets.mnist.load_data()
+    X_train = X_train / 255.0
+    X_test = X_test / 255.0
+    return X_train, y_train, X_test, y_test
+    
+'''
+Készíts egy neurális hálót, ami képes felismerni a kézírásos számokat.
+A háló kimenete legyen 10 elemű, és a softmax aktivációs függvényt használja.
+Hálon belül tetszőleges számú réteg lehet.
+Egy példa a kimenetre: model,
+return type: keras.engine.sequential.Sequential
+függvény neve: mnist_model
+'''
+def mnist_model():
+    model = tf.keras.Sequential([
+        tf.keras.layers.Flatten(input_shape=(28, 28)),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(10, activation='softmax')
+    ])
+    
+    return model
+'''
+Készíts egy metódust, ami a bemeneti hálot compile-olja.
+Optimizer: Adam
+Loss: SparseCategoricalCrossentropy(from_logits=False)
+Egy példa a bemenetre: model
+Egy példa a kimenetre: model
+return type: keras.engine.sequential.Sequential
+függvény neve: model_compile
+'''
+def model_compile(model):
+    optimizer = Adam()
+    loss = SparseCategoricalCrossentropy(from_logits=False)
+    model.compile(optimizer=optimizer, loss=loss) #, metrics=['accuracy']
+    return model
+'''
+Készíts egy metódust, ami a bemeneti hálót feltanítja.
+Egy példa a bemenetre: model,epochs, train_images, train_labels
+Egy példa a kimenetre: model
+return type: keras.engine.sequential.Sequential
+függvény neve: model_fit
+'''
+def model_fit(model, epochs, train_images, train_labels):
+    history = model.fit(train_images, train_labels, epochs=epochs) #, validation_split=0.2
+    return model
+
+'''
+Készíts egy metódust, ami a bemeneti hálót kiértékeli a teszt adatokon.
+Egy példa a bemenetre: model, test_images, test_labels
+Egy példa a kimenetre: test_loss, test_acc
+return type: float, float
+függvény neve: model_evaluate
+'''
+def model_evaluate(model, test_images, test_labels):
+    test_loss, test_acc = model.evaluate(test_images, test_labels) #, verbose=2
+    return test_loss, test_acc
